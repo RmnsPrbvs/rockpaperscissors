9a26b0394f682db061a5e92b79e1804285404325
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-04-21 16:17:54 -0700

    bot input added

--- code.js
+++ code.js
@@ -4,8 +4,25 @@ let buttons = document.querySelector(".buttons")
 
 let arr = [...(buttons.children)]
 
+let round = (input) => {
+    let botn = Math.round(Math.random() * 2)
+    if (botn == 0) {
+        bot_input = 'paper'
+    } else if (botn == 1) {
+        bot_input = 'rock'
+    } else {
+        bot_input = 'paper'
+    }
+
+    if (input == bot_input) {
+        console.log(input)
+    } else {
+        console.log(bot_input, input)
+    }
+}
+
 console.log(arr)
 for (child in arr) {
-    arr[child].addEventListener("click", (e) => {console.log('hello', e.target.innerText)})
+    arr[child].addEventListener("click", (e) => {round(e.target.innerText)})
 }
 
